2419ced7e4bba92094b09c3e1f3fa095ae25767c
Agustin <agusnespral@gmail.com>
2025-03-01 14:21:14 -0300

    Created test cases: - Validate website is displayed correctly - Validate contact form is displayed

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,21 @@
+import { HomePage } from "../support/pageObjects/HomePage";
+
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+  beforeEach(() => {
+    cy.visit('')
+    cy.manageCookies()
+  });
+
+  it('Validate website is displayed correctly,', () => {
+    cy.url().should('eq', Cypress.config('baseUrl'));
+    HomePage.locators.qubikaLogo().should('be.visible')
   })
+
+  it('Validate contact form is displayed', () => {
+    
+    HomePage.clickContactUsButton()
+    HomePage.locators.firstNameField().should('be.visible')
+    HomePage.locators.emailField().should('be.visible')
+    HomePage.locators.submitButton().scrollIntoView().should('be.visible')
+  });
 })
