f7d1be99b1d29a75b0d4a0f6d60821ee07edc1fb
Agustin <agusnespral@gmail.com>
2025-03-02 09:54:59 -0300

    created a fixture, added fixture to test case, changed arrow functions to regular function

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,19 +2,20 @@ import { HomePage } from "../support/pageObjects/HomePage";
 
 describe('Technical test - Excercise 1', () => {
   //step 1 from worksheet
-  beforeEach(() => {
+  beforeEach(function() {
     cy.visit('')
     cy.manageCookies()
+    cy.fixture('data.json').as('data')
   });
 
   //steps  2a, 2b
-  it('Validate website is displayed correctly', () => {
+  it('Validate website is displayed correctly', function() {
     cy.url().should('eq', Cypress.config('baseUrl'));
     HomePage.locators.qubikaLogo().should('be.visible')
   })
 
   //steps 3, 3a, 3b, 3c
-  it('Validate contact form is displayed successfully', () => { 
+  it('Validate contact form is displayed successfully', function() { 
     HomePage.clickContactUsButton()
     HomePage.locators.firstNameField().should('be.visible')
     HomePage.locators.emailField().should('be.visible')
@@ -22,19 +23,19 @@ describe('Technical test - Excercise 1', () => {
   });
 
   //steps 5, 6, 7, 8
-  it('Validate error messages are displayed on incompleted fields in contact form', () => { 
+  it('Validate error messages are displayed on incompleted fields in contact form', function() { 
     HomePage.clickContactUsButton()
     HomePage.locators.submitButton().scrollIntoView().click()
     HomePage.validateEmptyFieldErrorMessages()
-    HomePage.locators.contactUsModalFirstNameField().type('Test name')
+    HomePage.locators.contactUsModalFirstNameField().type(this.data.firstName)
     HomePage.validateEmptyFieldErrorMessages()
   });
 
   //steps 9, 10, 11, 12  
   it('Validate contact form is closed properly and data completed is retained after closing and reopening form', 
-    () => { 
+    function() { 
     HomePage.clickContactUsButton()
-    HomePage.locators.contactUsModalFirstNameField().type('Test name')
+    HomePage.locators.contactUsModalFirstNameField().type(this.data.firstName)
     HomePage.locators.submitButton().scrollIntoView().click()
     HomePage.clickCloseContactUsModal()
     HomePage.locators.contactUsModal().should('not.exist')
