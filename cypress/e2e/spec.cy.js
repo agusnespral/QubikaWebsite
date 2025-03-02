b9b8bdceebf6efd818995734c4909a8b32f4c0be
Agustin <agusnespral@gmail.com>
2025-03-02 09:40:08 -0300

    Created a function for validating error messages on POM Added test cases to spec

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,21 +1,48 @@
 import { HomePage } from "../support/pageObjects/HomePage";
 
-describe('template spec', () => {
+describe('Technical test - Excercise 1', () => {
+  //step 1 from worksheet
   beforeEach(() => {
     cy.visit('')
     cy.manageCookies()
   });
 
-  it('Validate website is displayed correctly,', () => {
+  //steps  2a, 2b
+  it('Validate website is displayed correctly', () => {
     cy.url().should('eq', Cypress.config('baseUrl'));
     HomePage.locators.qubikaLogo().should('be.visible')
   })
 
-  it('Validate contact form is displayed', () => {
-    
+  //steps 3, 3a, 3b, 3c
+  it('Validate contact form is displayed successfully', () => { 
     HomePage.clickContactUsButton()
     HomePage.locators.firstNameField().should('be.visible')
     HomePage.locators.emailField().should('be.visible')
     HomePage.locators.submitButton().scrollIntoView().should('be.visible')
   });
+
+  //steps 5, 6, 7, 8
+  it('Validate error messages are displayed on incompleted fields in contact form', () => { 
+    HomePage.clickContactUsButton()
+    HomePage.locators.submitButton().scrollIntoView().click()
+    HomePage.validateEmptyFieldErrorMessages()
+    HomePage.locators.contactUsModalFirstNameField().type('Test name')
+    HomePage.validateEmptyFieldErrorMessages()
+  });
+
+  //steps 9, 10, 11, 12  
+  it('Validate contact form is closed properly and data completed is retained after closing and reopening form', 
+    () => { 
+    HomePage.clickContactUsButton()
+    HomePage.locators.contactUsModalFirstNameField().type('Test name')
+    HomePage.locators.submitButton().scrollIntoView().click()
+    HomePage.clickCloseContactUsModal()
+    HomePage.locators.contactUsModal().should('not.exist')
+    HomePage.validateEmptyFieldErrorMessages()
+  });
+
+
+
+
+
 })
--- cypress/support/pageObjects/HomePage.js
+++ cypress/support/pageObjects/HomePage.js
@@ -1,13 +1,29 @@
 class homePage {
 
     locators = {
-        qubikaLogo: ()=> cy.get('.main-header .logo'),
-        contactUsButton: ()=> cy.get('.button.rounded.big-btn.contact-us-modal-open').contains('Contact Us'),
-        firstNameField: ()=> cy.get('#firstname-5e204c31-ede2-4976-a096-6919a081b2df'),
-        emailField: ()=> cy.get('#email-5e204c31-ede2-4976-a096-6919a081b2df').should('be.visible'),
-        submitButton: ()=> cy.get('.hs-button')
-
+        qubikaLogo: () => cy.get('.main-header .logo'),
+        contactUsButton: () => cy.get('.button.rounded.big-btn.contact-us-modal-open').contains('Contact Us'),
+        firstNameField: () => cy.get('#firstname-5e204c31-ede2-4976-a096-6919a081b2df'),
+        emailField: () => cy.get('#email-5e204c31-ede2-4976-a096-6919a081b2df').should('be.visible'),
+        submitButton: () => cy.get('.hs-button'),
+        closeButtonContactUs: () => cy.get('.modal-scroller .icon-x'),
+        contactUsModal: () => cy.get('.modal contact-us-modal show'),
+        contactUsModalFirstNameField: () => cy.get('.hs_firstname'),
+        contactUsModalLastNameField: () => cy.get('.hs_lastname'),
+        contactUsModalEmailField: () => cy.get('.hs_email'),
+        contactUsModalCompanyField: () => cy.get('.hs_company'),
+        contactUsModalContactTypeField: () => cy.get('.hs_contact_type'),
+        contactUsModalMessageField: () => cy.get('.hs_message'),
     }
+
+    contactUsKeys = [
+        'contactUsModalFirstNameField',
+        'contactUsModalLastNameField',
+        'contactUsModalEmailField',
+        'contactUsModalCompanyField',
+        'contactUsModalContactTypeField',
+        'contactUsModalMessageField'
+    ];
 
     //methods
 
@@ -17,8 +33,28 @@ class homePage {
 
     clickContactUsButton() {
         this.locators.contactUsButton().click();
+    };
+
+    clickCloseContactUsModal() {
+        this.locators.closeButtonContactUs().click();
+    };
+
+    validateEmptyFieldErrorMessages() {
+        this.contactUsKeys.forEach((key) => {
+          this.locators[key]().then(($field) => {
+            // Look for the error message element within the field using the specified selector
+            const $errorElement = $field.find('.hs-error-msg.hs-main-font-element');
+            if ($errorElement.length > 0) {
+              cy.wrap($errorElement)
+                .should('have.text', 'Please complete this required field.');
+            } else {
+              cy.log(`Field ${key} is complete.`);
+            }
+          });
+        });
+      }
     }
 
-}
+
 
 export const HomePage = new homePage
